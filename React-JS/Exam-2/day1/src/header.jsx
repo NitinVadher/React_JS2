cba7ccd3e35e921620dfb8a76892f0a6d36cf7d9
NitinVadher <nitinvadher963@gmail.com>
2024-12-03 16:24:16 +0530

    exam

---
+++ React-JS/Exam-2/day1/src/header.jsx
@@ -0,0 +1,205 @@
+import './App.css';
+
+function Header() {
+  return (
+    <div>
+   
+<header>
+  <div className="logo">
+    <img id='s' src="https://cdn.masaischool.com/masai-website/Masai_Logo_dark_web_b21aab8c62.webp" alt="" />
+  </div>
+  <div className="n">COURSES <i class="fa-solid fa-chevron-down"></i></div>
+
+  <nav>
+    <ul>
+      
+      <li> SCHOLARSHIP <i class="fa-solid fa-chevron-down"></i></li>
+      <li> SUCCESS STORIES</li>
+      <li> EVENTS</li>
+      <li> HIRE FROM US</li>
+      <li>FEES</li>
+      <button>R E G I S T E R</button>
+    </ul>
+  </nav>
+</header>
+<br />
+<center>
+<div className="a">
+    <h1>The Masai Team</h1>
+    <p>Behind every successful start-up, there is a seasoned and an experienced team. Unified by a mission to make Masai India’s biggest career focussed higher education institute, the Masai family currently has over 200+ members, with the team getting bigger every week.</p>
+</div>
+</center>
+
+<center>
+<div className="b">
+    <h1>Leadership Team</h1>
+    <p>With more than 70 years of combined experience in technology, engineering and design our Executive Team has one common goal - “Unlock the human potential of India by transforming the education system”</p>
+</div>
+</center>
+<br />
+
+        <div className=" flex h-[300px] w-[90%] ms-[5%] justify-between items-center mt-[50px]">
+
+            <div id="c1" className="border rounded-[20px] h-[300px] w-[20%]  text-center text-[white] ">
+                <h6 className="mt-[90%] text-[20px] font-[700]"> 
+                Prateek Shukla
+                </h6>
+                <p> Founder & CEO</p>
+                {/* <i class="fa-brands fa-linkedin text-[#1265BF] "    ></i> */}
+                <i class="fa-brands fa-linkedin-in  text-[#1265BF]   text-[19px] "></i>
+                <i class="fa-brands fa-twitter   text-[#1265BF]      text-[19px] ms-[10px] "></i>
+
+
+            </div>
+            <div id="c2" className="border rounded-[20px] h-[300px] w-[20%]   text-center text-[white]">
+            <h6 className="mt-[90%] text-[20px] font-[700]"> 
+                Prateek Shukla
+                </h6>
+                <p> Founder & CEO</p>
+                {/* <i class="fa-brands fa-linkedin text-[#1265BF] "    ></i> */}
+                <i class="fa-brands fa-linkedin-in  text-[#1265BF]   text-[19px] "></i>
+                <i class="fa-brands fa-twitter   text-[#1265BF]      text-[19px] ms-[10px] "></i>
+
+
+            </div>
+            <div id="c3" className="border rounded-[20px] h-[300px] w-[20%]   text-center text-[white]">
+            <h6 className="mt-[90%] text-[20px] font-[700]"> 
+                Prateek Shukla
+                </h6>
+                <p> Founder & CEO</p>
+                {/* <i class="fa-brands fa-linkedin text-[#1265BF] "    ></i> */}
+                <i class="fa-brands fa-linkedin-in  text-[#1265BF]   text-[19px] "></i>
+                <i class="fa-brands fa-twitter   text-[#1265BF]      text-[19px] ms-[10px] "></i>
+
+
+            </div>
+            <div id="c4" className="border rounded-[20px] h-[300px] w-[20%]  text-center text-[white] ">
+            <h6 className="mt-[90%] text-[20px] font-[700]"> 
+                Prateek Shukla
+                </h6>
+                <p> Founder & CEO</p>
+                {/* <i class="fa-brands fa-linkedin text-[#1265BF] "    ></i> */}
+                <i class="fa-brands fa-linkedin-in  text-[#1265BF]   text-[19px] "></i>
+                <i class="fa-brands fa-twitter   text-[#1265BF]      text-[19px] ms-[10px] "></i>
+
+
+            </div>
+
+        </div>
+
+        <div className=" flex h-[300px] w-[90%] ms-[5%] justify-between items-center mt-[50px]">
+
+            <div id="c11" className="border rounded-[20px] h-[300px] w-[20%]  text-center text-[white] ">
+            <h6 className="mt-[90%] text-[20px] font-[700]"> 
+                Prateek Shukla
+                </h6>
+                <p> Founder & CEO</p>
+                {/* <i class="fa-brands fa-linkedin text-[#1265BF] "    ></i> */}
+                <i class="fa-brands fa-linkedin-in  text-[#1265BF]   text-[19px] "></i>
+                <i class="fa-brands fa-twitter   text-[#1265BF]      text-[19px] ms-[10px] "></i>
+
+
+            </div>
+            <div id="c22" className="border rounded-[20px] h-[300px] w-[20%]   text-center text-[white]">
+            <h6 className="mt-[90%] text-[20px] font-[700]"> 
+                Prateek Shukla
+                </h6>
+                <p> Founder & CEO</p>
+                {/* <i class="fa-brands fa-linkedin text-[#1265BF] "    ></i> */}
+                <i class="fa-brands fa-linkedin-in  text-[#1265BF]   text-[19px] "></i>
+                <i class="fa-brands fa-twitter   text-[#1265BF]      text-[19px] ms-[10px] "></i>
+
+
+            </div>
+            <div id="c33" className="border rounded-[20px] h-[300px] w-[20%]   text-center text-[white]">
+            <h6 className="mt-[90%] text-[20px] font-[700]"> 
+                Prateek Shukla
+                </h6>
+                <p> Founder & CEO</p>
+                {/* <i class="fa-brands fa-linkedin text-[#1265BF] "    ></i> */}
+                <i class="fa-brands fa-linkedin-in  text-[#1265BF]   text-[19px] "></i>
+                <i class="fa-brands fa-twitter   text-[#1265BF]      text-[19px] ms-[10px] "></i>
+
+
+            </div>
+            <div id="c44" className="border rounded-[20px] h-[300px] w-[20%]  text-center text-[white] ">
+            <h6 className="mt-[90%] text-[20px] font-[700]"> 
+                Prateek Shukla
+                </h6>
+                <p> Founder & CEO</p>
+                {/* <i class="fa-brands fa-linkedin text-[#1265BF] "    ></i> */}
+                <i class="fa-brands fa-linkedin-in  text-[#1265BF]   text-[19px] "></i>
+                <i class="fa-brands fa-twitter   text-[#1265BF]      text-[19px] ms-[10px] "></i>
+
+
+            </div>
+
+        </div>
+<br />
+<footer class="bg-[black] text-white rounded-t-[5%] mt-[100px]">
+        <div className="f1 p-3 flex justify-center items-center border-[#454041] border-b h-[100px]">
+                <h1 class="text-[1.3vw] font-bold">Building your tech skills has never been easier.</h1>
+                <button class="bg-[red] text-white p-2 rounded-lg ms-3 w-[170px]">START LEARNING</button>
+        </div>
+        <div className="f2 flex justify-between items-center w-[80%] m-[auto] p-3 border-[#454041] border-b">
+        <div className="logo h-full flex justify-center items-center">
+                <img src="https://www.masaischool.com/images/footer/masai-logo-dark.svg" alt=""/>
+            </div>
+            <div className="icon flex itens-center h-full">
+                <span class="text-[#808080] font-medium">Follow us -</span>
+                <i class="fa-brands fa-x-twitter text-[#808080] ms-5 text-[22px]"></i>
+                <i class="fa-brands fa-facebook text-[#808080] ms-5 text-[22px]"></i>
+                <i class="fa-brands fa-linkedin text-[#808080] ms-5 text-[22px]"></i>
+                <i class="fa-brands fa-instagram text-[#808080] ms-5 text-[22px]"></i>
+                <i class="fa-brands fa-github text-[#808080] ms-5 text-[22px]"></i>
+                <i class="fa-brands fa-youtube text-[#808080] ms-5 text-[22px]"></i>
+            </div>
+        </div>
+        <div className="f3 w-[80%] m-[auto] grid grid-cols-4 gap-4">
+            <div className="f33 p-4 ">
+                <h1 class="font-medium text-[20px]">Masai Courses</h1>
+                <p class="mt-2 font-medium">Software Development - Full Time
+                Data Analytics - Full Time</p>
+                <h1 class="font-medium mt-4 text-[20px]">IIT For All Courses</h1>
+                <p class="mt-2 font-medium">IIT Mandi - Minor in CSE & Adv. Technologies</p>
+                <p class="mt-2 font-medium">IIT Ropar - Minor in A</p>
+            </div>
+            <div className="f33 p-4 ">
+                <p class="mt-2 font-medium">Success Stories</p>
+                <p class="mt-2 font-medium">Our Team</p>
+                <p class="mt-2 font-medium">Careers</p>
+                <p class="mt-2 font-medium">Masai Blog</p>
+                <p class="mt-2 font-medium">Talent Circle</p>
+                <p class="mt-2 font-medium">Masai School Admissions Test (MSAT)</p>
+            </div>
+            <div className="f33 p-4 ">
+                <p class="mt-2 font-medium">Hire From Us</p>
+                <p class="mt-2 font-medium">Our Investors</p>
+                <p class="mt-2 font-medium">Newsroom</p>
+                <p class="mt-2 font-medium">Contact Us</p>
+                <p class="mt-2 font-medium">Become Coach</p>
+            </div>
+            <div className="f33 p-4 ">
+                <p class="mt-2 font-medium">About Us</p>
+                <p class="mt-2 font-medium">FAQ</p>
+                <p class="mt-2 font-medium">Referral Program</p>
+                <p class="mt-2 font-medium">Masai Learn</p>
+                <p class="mt-2 font-medium">Industry Mentors</p>
+            </div>
+        </div>
+        <div className="f4 flex justify-end items-center w-[80%] m-[auto] border-[#454041] border-b h-[150px]">
+            <button>Connect with a growing<br/>
+            community of learners</button>
+            <button class="text-[#27A8E8] bg-[#313131] rounded-lg w-[250px] h-[50px] ms-5 flex items-center justify-center"><i class="fa-brands fa-telegram text-[#27A8E8] text-[25px] me-2"></i> JOIN OUR TELEGRAM</button>
+        </div>
+        <div className="f5 w-[80%] flex justify-between items-center m-[auto] h-[80px]">
+            <p class="text-[#707070] uppercase">Copyright by @NitinVadher</p>
+            <p class="text-[10px] underline">Privacy Policy & Terms and Conditions</p>
+        </div>
+    </footer>
+
+</div>
+  );
+}
+
+export default Header;
